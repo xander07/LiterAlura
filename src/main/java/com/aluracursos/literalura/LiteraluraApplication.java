c7785f74c64232520c4db4a437001feea5a92a6e
Johan Alexander Paniagua Bedoya <japaniaguab@unal.edu.co>
2024-07-16 12:07:12 -0500

    Ejecutar menú principal

--- src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
+++ src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
@@ -1,13 +1,25 @@
 package com.aluracursos.literalura;
 
+import com.aluracursos.literalura.principal.Principal;
+import com.aluracursos.literalura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiteraluraApplication {
 
+	@Autowired
+	private LibroRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.mostrarMenu();
 	}
 
 }
